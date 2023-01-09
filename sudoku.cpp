476659f64f3edd4d19e5c6b7915ba29566eaccc2
amanpandey-03 <amanpandeyjsr307@gmail.com>
2023-01-10 02:23:20 +0530

    commit

---
+++ sudoku.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int i, j , k;
+	cout <<"enter the number of rows : "<<endl;
+	for (i=1;i<=k;i++){
+		for(j=1;j<=(k/2)+i;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
